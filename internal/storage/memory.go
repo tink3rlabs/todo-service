7b584c96159eed92ddd2f305ef7dc02f918e9f53
Dean Efrati <deanefrati@users.noreply.github.com>
2024-10-08 06:21:41 -0400

    feat: Generalized storage interface (#37)

--- internal/storage/memory.go
+++ internal/storage/memory.go
@@ -4,15 +4,15 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"reflect"
+	"strings"
 	"sync"
-
-	"todo-service/types"
 )
 
 var memoryAdapterLock = &sync.Mutex{}
 
 type MemoryAdapter struct {
-	todos []types.Todo
+	store map[string][]interface{}
 }
 
 var memoryAdapterInstance *MemoryAdapter
@@ -22,7 +22,7 @@ func GetMemoryAdapterInstance() *MemoryAdapter {
 		memoryAdapterLock.Lock()
 		defer memoryAdapterLock.Unlock()
 		if memoryAdapterInstance == nil {
-			memoryAdapterInstance = &MemoryAdapter{todos: []types.Todo{}}
+			memoryAdapterInstance = &MemoryAdapter{store: map[string][]interface{}{}}
 		}
 	}
 	return memoryAdapterInstance
@@ -36,50 +36,64 @@ func (m *MemoryAdapter) Ping() error {
 	return nil
 }
 
-func (m *MemoryAdapter) ListTodos(limit int, cursor string) ([]types.Todo, string, error) {
-	todos := []types.Todo{}
+func (m *MemoryAdapter) Create(item any) error {
+	itemType := reflect.TypeOf(item).String()
+	m.store[itemType] = append(m.store[itemType], item)
+	return nil
+}
+
+func (m *MemoryAdapter) Get(dest any, itemKey string, itemValue string) error {
+	t := strings.ReplaceAll(reflect.TypeOf(dest).String(), "*", "")
+	for _, item := range m.store[t] {
+		if reflect.ValueOf(item).FieldByName(itemKey).String() == itemValue {
+			v := reflect.ValueOf(dest)
+			// Check if the value is a pointer and if it's settable
+			if v.Kind() == reflect.Ptr && v.Elem().CanSet() {
+				v.Elem().Set(reflect.ValueOf(item))
+			}
+			return nil
+		}
+	}
+	return ErrNotFound
+}
+
+func (m *MemoryAdapter) Delete(item any, itemKey string, itemValue string) error {
+	t := strings.ReplaceAll(reflect.TypeOf(item).String(), "*", "")
+	for k, v := range m.store[t] {
+		if reflect.ValueOf(v).FieldByName(itemKey).String() == itemValue {
+			m.store[t] = append(m.store[t][:k], m.store[t][k+1:]...)
+		}
+	}
+	return nil
+}
+
+func (m *MemoryAdapter) List(items any, itemKey string, limit int, cursor string) (string, error) {
 	nextId := ""
 
 	id, err := base64.StdEncoding.DecodeString(cursor)
 	if err != nil {
-		return todos, "", fmt.Errorf("failed to decode next cursor: %v", err)
+		return "", fmt.Errorf("failed to decode next cursor: %v", err)
 	}
 
+	r := reflect.ValueOf(items)
+
 	// Get one extra item to be able to set that item's Id as the cursor for the next request
-	for _, v := range m.todos {
-		if (v.Id >= string(id)) && len(todos) < limit+1 {
-			todos = append(todos, v)
+	t := strings.ReplaceAll(r.Elem().Type().String(), "[]", "")
+	for _, v := range m.store[t] {
+		if (reflect.ValueOf(v).FieldByName(itemKey).String() >= string(id)) && r.Elem().Len() < limit+1 {
+			r.Elem().Set(reflect.Append(r.Elem(), reflect.ValueOf(v)))
 		}
 	}
 
 	// If we have a full list, set the Id of the extra last item as the next cursor and remove it from the list of items to return
-	if len(todos) == limit+1 {
-		nextId = base64.StdEncoding.EncodeToString([]byte(todos[len(todos)-1].Id))
-		todos = todos[:len(todos)-1]
-	}
-
-	return todos, nextId, nil
-}
-
-func (m *MemoryAdapter) GetTodo(id string) (types.Todo, error) {
-	for _, v := range m.todos {
-		if v.Id == id {
-			return v, nil
+	if (r.Elem().Len()) == limit+1 {
+		lastItem := r.Elem().Index(r.Elem().Len() - 1)
+		nextId = base64.StdEncoding.EncodeToString([]byte(lastItem.FieldByName(itemKey).String()))
+		// Check if the value is a pointer and if it's settable
+		if r.Kind() == reflect.Ptr && r.Elem().CanSet() {
+			r.Elem().Set(r.Elem().Slice(0, r.Elem().Len()-1))
 		}
 	}
-	return types.Todo{}, ErrNotFound
-}
 
-func (m *MemoryAdapter) DeleteTodo(id string) error {
-	for k, v := range m.todos {
-		if v.Id == id {
-			m.todos = append(m.todos[:k], m.todos[k+1:]...)
-		}
-	}
-	return nil
-}
-
-func (m *MemoryAdapter) CreateTodo(todo types.Todo) error {
-	m.todos = append(m.todos, todo)
-	return nil
+	return nextId, nil
 }
